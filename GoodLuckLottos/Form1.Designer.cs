04ddd005f50762985127a531c9c7dfe0b6854615
YeJuneJi <darksuhak7@gmail.com>
2018-12-03 16:09:32 +0900

    Xml변환하기위한 버튼 생성

--- GoodLuckLottos/Form1.Designer.cs
+++ GoodLuckLottos/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.btnMenu7 = new System.Windows.Forms.Button();
             this.btnOcrPerSec = new System.Windows.Forms.Button();
             this.btnStatistics = new System.Windows.Forms.Button();
+            this.btnXml = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.lottoGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -140,11 +141,21 @@
             this.btnStatistics.UseVisualStyleBackColor = true;
             this.btnStatistics.Click += new System.EventHandler(this.btnStatistics_Click);
             // 
+            // btnXml
+            // 
+            this.btnXml.Location = new System.Drawing.Point(549, 375);
+            this.btnXml.Name = "btnXml";
+            this.btnXml.Size = new System.Drawing.Size(123, 23);
+            this.btnXml.TabIndex = 20;
+            this.btnXml.Text = "Xml변환";
+            this.btnXml.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 463);
+            this.Controls.Add(this.btnXml);
             this.Controls.Add(this.btnStatistics);
             this.Controls.Add(this.btnOddeorEven);
             this.Controls.Add(this.btnMenu7);
@@ -178,6 +189,7 @@
 
         private System.Windows.Forms.Button btnOcrPerSec;
         private System.Windows.Forms.Button btnStatistics;
+        private System.Windows.Forms.Button btnXml;
     }
 }
 
